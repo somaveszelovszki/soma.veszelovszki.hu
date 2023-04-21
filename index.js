9ec713df449bf942b033fa06f8e79ee7d9b18050
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-04-21 21:25:42 +0200

    Random joke card added

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ $(document).ready (function() {
     })
 
     $('[data-calculate-years-since]').each(function() {
-        var elapsedTime = new Date(new Date() - Date.parse($(this).data('calculate-years-since')))
+        let elapsedTime = new Date(new Date() - Date.parse($(this).data('calculate-years-since')))
         $(this).text(Math.abs(elapsedTime.getUTCFullYear() - 1970));
     })
+
+    $('.joke').each(function() {
+        fetch("https://v2.jokeapi.dev/joke/any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
+            .then((info) => info.json())
+            .then((item) => {
+                $(this).find('.setup').text(item.setup)
+                $(this).find('.delivery').text(item.delivery)
+            });
+    });
 });
