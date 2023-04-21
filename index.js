340761622264369d00e45a18cf19b2df8abbae0b
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-04-21 18:54:20 +0200

    About me and Projects sections added

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ $(document).ready (function() {
     $(".animated-width").each(function() {
         $(this).css("width", $(this).data("target") + "%")
     });
+
+    $('[data-link]').click(function() {
+        window.location.href = $(this).data("link");
+    })
+
+    $('[data-calculate-years-since]').each(function() {
+        var elapsedTime = new Date(new Date() - Date.parse($(this).data('calculate-years-since')))
+        $(this).text(Math.abs(elapsedTime.getUTCFullYear() - 1970));
+    })
 });
